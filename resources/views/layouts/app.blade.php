117812b1e012303335792e56e56bd06426ecaf6c
regzimarx <gergimarjohnalinsangao@gmail.com>
2022-03-17 09:29:38 +0800

    Make alpine.js offline

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
     <link rel="stylesheet" href="{{ asset('css/tailwind.output.css') }}" />
-    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+    <script src="{{ asset('js/alpine.min.js') }}"></script>
     <script src="{{ asset('js/init-alpine.js') }}" defer></script>
     <script src="{{ asset('js/app.js') }}" defer></script>
 
@@ -38,8 +38,7 @@
         @stack('modals')
 
         @livewireScripts
-        <script src="https://cdn.jsdelivr.net/gh/livewire/turbolinks@v0.1.x/dist/livewire-turbolinks.js"
-                data-turbolinks-eval="false" data-turbo-eval="false"></script>
+        <script src="{{ asset('js/alpine-turbolinks.js') }}"></script>
     </div>
 </body>
 
